548df5ebd9638c8efb646358ccbd7fc5f6310464
Sourav <sourav@Souravs-MacBook-Air.local>
2022-12-29 20:23:39 +0530

    create project

---
+++ NetflixUsingSwift5andUIKitWithoutStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixUsingSwift5andUIKitWithoutStoryboard
+//
+//  Created by Sourav on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
