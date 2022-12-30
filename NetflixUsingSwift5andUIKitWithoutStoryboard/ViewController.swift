ed682d7bd1c5f1c68064ff7cd6d6e36fec8d011c
Sourav <sourav@Souravs-MacBook-Air.local>
2022-12-30 14:37:38 +0530

    add code for preview for  ViewController

--- NetflixUsingSwift5andUIKitWithoutStoryboard/ViewController.swift
+++ NetflixUsingSwift5andUIKitWithoutStoryboard/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
 
 }
 
+
+
+struct ViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+            .ignoresSafeArea()
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerPreview.ContainerView>) -> some UIViewController {
+            
+            return ViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewControllerPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerPreview.ContainerView>) {
+            
+        }
+    }
+}
